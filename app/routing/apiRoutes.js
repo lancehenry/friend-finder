75cde77f39a7b9ba989b39bdb9937a430ef4c147
lschmittling <lanceschmittling@Lances-MBP>
2018-04-20 09:23:16 -0500

    Working on functionality to calculate the best friend

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,21 +1,28 @@
 // Load Data - Linking our routes to "data" sources.
 // Reference "Hot Restaurants class exercise"
-var friends = require('../data/friends');
+var friends = require("../data/friends");
 
 // Routing - API Get Requests
 module.exports = function(app) {
-
-  app.get('/api/friends', function(req, res) {
+  app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
 
-  app.post('/api/friends', function(req, res) {
-      friends.push(req.body);
-      res.json(true);
+  app.post("/api/friends", function(req, res) {
+
+    // Set variables from users input fields
+    var userData = req.body;
+
+    // Loops through the friends currently in database
+    for (var i = 0; i < friends.length; i++) {
+      var scoreDifference = Math.abs(parseInt(userData.scores[i]) - parseInt(friends.scores[i]));
+
+      console.log(scoreDifference);
+      
+      };
+
+    friends.push(userData);
+    res.json(true);
+
   });
 };
-
-// Need to write a for loop to go through friends and calculate the scores from input and compare those to the other friends scores in the friends.js file, return closest
-for (var i = 0; i < friends.length; i++) {
-
-}
