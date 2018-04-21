605b6ea1a0e0afc1fde9747bea59810003394969
lschmittling <lanceschmittling@Lances-MBP>
2018-04-21 16:50:00 -0500

    I think I got the functionality working for best friend match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,7 +9,6 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
-
     // Set variables from users input fields
     var userData = req.body;
     // console.log(userData);
@@ -18,33 +17,34 @@ module.exports = function(app) {
     var userScore = userData.scores;
     // console.log(userScore);
 
-    // Per Jay:
-    // Lowest score variable, don't set to anything need to then compare those together. Push the closest friend to an object. This is what will then get returned to the DOM.
-    var bestFriend = {
-      name: '',
-      photo: '',
-      scores: []
-    };
-
     // An array to hold the scores
     var scoreArray = [];
     var totalDifference = 0;
+    var bestFriend = 0;
 
     // Loops through the friends currently in database
     for (var i = 0; i < friends.length; i++) {
-      
       // Need to put another loop inside this loop in order to go through the scores array
       for (var j = 0; j < userScore.length; j++) {
-        
-        totalDifference = Math.abs(parseInt(friends[i].scores[i]) - parseInt(userScore[j]));
-        
+        totalDifference = Math.abs(
+          parseInt(friends[i].scores[i]) - parseInt(userScore[j])
+        );
       }
       scoreArray.push(totalDifference);
-      };
+    }
+
+    // Per Jay:
+    // After the totalDifference is set to an array, create another loop to then find the closest match. This is what will then get returned to the DOM.
+    for (var i = 0; i < scoreArray.length; i++) {
+      if (scoreArray[i] <= scoreArray[bestFriend]) {
+        bestFriend = i;
+      }
+    }
+
+    var bestMatch = friends[bestFriend];
+    res.json(bestMatch);
+    // console.log(bestMatch.name + '\n' + bestMatch.photo);
 
     friends.push(userData);
-    
-    res.json(true);
-
   });
 };
